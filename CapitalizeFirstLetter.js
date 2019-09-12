3f5aff517b351f6161634e7d4ac8a1ef443551e1
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-09-12 11:27:13 -0500

    Solved Capitalize First Letter

---
+++ CapitalizeFirstLetter.js
@@ -0,0 +1,16 @@
+// Letter Capitalize 
+// Have the function LetterCapitalize(str) take the str parameter being passed and capitalize the first
+// letter of each word.  Words will be separated by only one space.
+// Use the Parameter Testing feature in the box below to test your code with different arguments.
+
+function LetterCapitalize(str) { 
+    //I first created a new variable then split it.  I then mapped the first letter of each new string and made that character upper case
+    //I then added that string to the rest of the string which was lowercased and joined them.  Finally I returned the new variable.
+    const newString = str.split(/\s+/).map( s => s.charAt( 0 ).toUpperCase() + s.substring(1).toLowerCase() ).join(" ");
+      // code goes here  
+      return newString; 
+             
+    }
+       
+    // keep this function call here 
+    LetterCapitalize(readline());
